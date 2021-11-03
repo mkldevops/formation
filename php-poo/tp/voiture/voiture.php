747553a8d520d15f96fc3dd65d10e7eb90f70685
Fahari Hamada Sidi <mkl.devops@gmail.com>
2021-11-03 15:55:54 +0100

    add first pages of sql formation

--- php-poo/tp/voiture/voiture.php
+++ php-poo/tp/voiture/voiture.php
@@ -1,13 +1,30 @@
 <?php
 
-class Voiture
+class Vehicule
+{
+  private int $vitesse = 120;
+
+  public function getVitesse(): int
   {
-    public static $nbPortes = 5;
-    
-    public static function ajouterPortes()
-    {
-      self::$nbPortes++;
-      echo sprintf("Une voiture avec %d portes", self::$nbPortes);
-    }
+    return $this->vitesse;
   }
-  var_dump(Voiture::$nbPortes, Voiture::ajouterPortes());
+
+  public function setVitesse(int $vitesse): self
+  {
+    $this->vitesse = $vitesse;
+
+    return $this;
+  }
+}
+
+class Voiture extends Vehicule
+{
+  public static $nbPortes = 5;
+  
+  public static function ajouterPortes()
+  {
+    self::$nbPortes++;
+    echo sprintf("Une voiture avec %d portes", self::$nbPortes);
+  }
+}
+var_dump(Voiture::$nbPortes, Voiture::ajouterPortes());
